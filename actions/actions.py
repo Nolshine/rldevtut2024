12d4e33f9b9bf31bb09f06b80a61c9ecbf94f74d
Nolshine <mtr131@gmail.com>
2024-08-01 19:58:39 +0100

    resolve potential memory leak from floating entities in the registry

--- actions/actions.py
+++ actions/actions.py
@@ -24,12 +24,14 @@ class Move:
         pos = entity.components[Position]
         target = Position(pos.x + self.dx, pos.y + self.dy)
         map_tiles = r[None].relation_tag[ActiveMap].components[Tiles]
+        if map_tiles[target.x, target.y] == TileIndices.WALL:
+            return
         entities = r.Q.all_of(tags=[IsActor, IsBlocking], relations=[(InMap, map_)])
-        if ((not map_tiles[target.x, target.y] == TileIndices.WALL) and
-            (not any(e.components[Position].raw == target.raw for e in entities))):
-            entity.components[Position] = target
-        assert IsPlayer in entity.tags
-        update_fov(entity)
+        if any(e.components[Position].raw == target.raw for e in entities):
+            return
+        entity.components[Position] = target
+        if IsPlayer in entity.tags:
+            update_fov(entity)
 
 def escape_action(entity: tcod.ecs.Entity) -> None:
     raise SystemExit()
@@ -39,6 +41,8 @@ def regenenerate_map(entity: tcod.ecs.Entity) -> None: # TODO: remove when not i
     new_seed = int(time.time())
     print(f"Seed: {new_seed}")
     r[None].components["Random"].seed(new_seed)
+    (e.clear() for e in r.Q.all_of(relations=[(InMap, ...)]).none_of(tags=[IsPlayer]))
+    r[None].relation_tag[ActiveMap].clear()
     map_ = generate_caves(
         r,
         SCREEN_W,
