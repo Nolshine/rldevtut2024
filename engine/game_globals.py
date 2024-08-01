4f30c9894a4d86f224ca5f4d4b5a86c548f8a155
Nolshine <mtr131@gmail.com>
2024-08-01 10:16:11 +0100

    add clarifying comment in game_globals.py

--- engine/game_globals.py
+++ engine/game_globals.py
@@ -14,3 +14,10 @@ context: tcod.context.Context
 root_console: tcod.console.Console
 game_state: State
 world: tcod.ecs.Registry
+
+
+# For posterity: I was going to have these objects live here and not
+# get passed around as parameters but I forgot somewhere along the way
+# and the project is not structured to use this. These will remain here
+# just to keep main.py clean, but otherwise this file is a little useless.
+# Sorry if you cloned this repo and this file caused confusion!
